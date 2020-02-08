06d15efe7297dc0207fc21d7005d563b767496e8
Sopsy <721951+Sopsy@users.noreply.github.com>
2020-02-08 19:22:39 +0100

    Add .gitignore, update comment in sh script

--- update-blacklist.sh
+++ update-blacklist.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 # Example usage of the script with Nginx - run in cron
+#
 # Requires you to create IPBlacklist.php with the contents of the example in README.
+# Just change the file_put_contents into echo (it's not echo to prevent 20 megabytes of output)
+#
 
 SCRIPT=$(readlink -f "${0}")
 SCRIPTPATH=$(dirname "${SCRIPT}")
 
-/usr/bin/php ${SCRIPTPATH}/IPBlacklist.php > /etc/nginx/snippets/blacklisted-ips.conf
+/usr/bin/php ${SCRIPTPATH}/IPBlacklist.php > /etc/nginx/conf.d/blacklisted-ips.conf
 
+# Reload nginx to apply changes
 if /usr/sbin/nginx -t -q
 then
     /usr/sbin/service nginx reload
